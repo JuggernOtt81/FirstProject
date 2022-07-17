ba60bd7a7b923fb3ca698d79d6445bcc280bad89
juggernott81 <juggernott81@gmail.com>
2022-07-17 01:37:25 -0400

    back to logic and real programming! YAY!

--- n-cent-i-vize/app-js/app.js
+++ n-cent-i-vize/app-js/app.js
@@ -1,4 +1,62 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World!");
+
+//get the values from the page
+//start or controller function
+function getValues(){
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;    
+    let endValue = document.getElementById("endValue").value;
+
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+    
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue)){
+        //call generateNumbers()
+        let numbers = generateNumbers(startValue, endValue);
+        //call displayNumbers
+        displayNumbers(numbers);
+    }
+    else{
+        alert("YOU MUST ENTER INTEGERS!");
+    }
+}
+
+//generate numbers from the start value to the end value
+//logic function(s)
+function generateNumbers(startValue, endValue){
+    let numbers = [];
+    for (let i = startValue; i <= endValue; i++){
+        //this will execute in a loop until i = endValue
+        numbers.push(i);
+    }
+    return numbers;
+
+}
+
+//display the numbers, and mark the EVEN numbers BOLD
+//display or view function(s)
+function displayNumbers(numbers){
+    let templateRows = "";
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        
+        if(number % 2 == 0){
+            className = "even";
+        }else
+        {
+            className = "odd";
+        }
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+        
+
+
+/*         if(number % 2 == 0){
+            templateRows += `<tr><td><strong>${number}</strong></td></tr>`;
+        }else
+        {
+            templateRows += `<tr><td>${number}</td></tr>`;
+        } */
+    }
+    document.getElementById("results").innerHTML = templateRows;
 }
