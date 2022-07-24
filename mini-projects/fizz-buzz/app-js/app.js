a139fb24219130df21f7b5f20665d5b21abceb65
juggernott81 <juggernott81@gmail.com>
2022-07-24 12:30:11 -0400

    a few improvements

--- mini-projects/fizz-buzz/app-js/app.js
+++ mini-projects/fizz-buzz/app-js/app.js
@@ -6,57 +6,81 @@ function getValues() {
   let limitValue = document.getElementById("limitValue").value;
 
   //parse into Integers
-  fizzValue = parseInt(fizzValue);
-  buzzValue = parseInt(buzzValue);
-  limitValue = parseInt(limitValue);
+  fizzInt = parseInt(fizzValue);
+  buzzInt = parseInt(buzzValue);
+  limitInt = parseInt(limitValue);
 
   if (
-    Number.isInteger(fizzValue) &&
-    Number.isInteger(buzzValue) &&
-    Number.isInteger(limitValue)
+    Number.isInteger(fizzInt) &&
+    Number.isInteger(buzzInt) &&
+    Number.isInteger(limitInt)
   ) {
+    
     //define numbers by calling fizzBuzz funtion
-    fizzBuzz(fizzValue, buzzValue, limitValue);
-
-  } else {
+    let fbArray = fizzBuzz(fizzInt, buzzInt, limitInt);
+    
+  } 
+  else {
     alert("YOU MUST ENTER INTEGERS!");
   }
 }
 
 //build the array of results
-function fizzBuzz(fizzValue, buzzValue, limitValue) {
+function fizzBuzz(fizzInt, buzzInt, limitInt) {
   //init the returnArray
   let returnArray = [];
 
   //loop from 1-vl
-  let lv = limitValue;
-  let fb = fizzValue * buzzValue;
+  let lv = limitInt;
+  let fb = fizzInt * buzzInt;
   let i = 1;
   while (i <= lv) {
     if (i % fb == 0) {
       returnArray.push(`FIZZ <i class="fa-solid fa-bolt"></i> BUZZ`);
-    } else if (i % buzzValue == 0) {
+    } else if (i % buzzInt == 0) {
       returnArray.push(`<span">BUZZ</span>`);
-    } else if (i % fizzValue == 0) {
+    } else if (i % fizzInt == 0) {
       returnArray.push(`<span>FIZZ</span>`);
     } else {
       returnArray.push(i);
     }
     i++;
   }
-  //pass the array to the displayData function
-  displayData(returnArray);
+    return returnArray;
 }
 
 //define displayData
-function displayData(fbArray) {
-  //loop over the array create a tablerow for each item.
+function displayData(returnArray) {
+
+    //get the table body element from the page
+    let tableBody = document.getElementById("results");
+
+    //get the template row
+    let templateRow = document.getElementById("fbTemplate");
+
+    //clear table first
+    tableBody.innerHTML = "";
+    
+    for (let i = 0; i < returnArray.length; i+= buzzInt){
+        let tableRow = document.importNode(templateRow.contentEditable, true);
+        for (let x = 0; x < buzzInt; x++){
+        //grab use the to put into array
+        let rowCols = tableRow.querySelectorAll("td");
+        rowCols[x].textContent = returnArray[i];        
+        tableBody.appendChild(tableRow);
+        }
+    }
+
+
+/*   //loop over the array create a tablerow for each item.
   let templateRows = "";
-  for (let i = 0; i < fbArray.length; i++) {
-    templateRows += `<tr><td>${fbArray[i]}</td></tr>`;
+
+  for (let i = 0; i < returnArray.length; i++) {
+    templateRows += `<tr><td>${returnArray[i]}</td></tr>`;
   }
-  document.getElementById("results").innerHTML = templateRows;
+  document.getElementById("results").innerHTML = templateRows; */
 }
+
 
 function resetTable() {
   let templateRows = "";
