230bb8f81b22c6863f13f5a6c93a6ed2324298df
juggernott81 <juggernott81@gmail.com>
2022-07-25 01:09:32 -0400

    extra mile

--- mini-projects/fizz-buzz/app-js/app.js
+++ mini-projects/fizz-buzz/app-js/app.js
@@ -1,3 +1,4 @@
+
 //start or controller function(get the values from the page)
 function getValues() {
   //get the values from the page
@@ -6,39 +7,39 @@ function getValues() {
   let limitValue = document.getElementById("limitValue").value;
 
   //parse into Integers
-  fizzValue = parseInt(fizzValue);
-  buzzValue = parseInt(buzzValue);
-  limitValue = parseInt(limitValue);
+  fizzInt = parseInt(fizzValue);
+  buzzInt = parseInt(buzzValue);
+  limitInt = parseInt(limitValue);
 
+  //validate that entries are integers
   if (
-    Number.isInteger(fizzValue) &&
-    Number.isInteger(buzzValue) &&
-    Number.isInteger(limitValue)
+    Number.isInteger(fizzInt) &&
+    Number.isInteger(buzzInt) &&
+    Number.isInteger(limitInt)
   ) {
     //define numbers by calling fizzBuzz funtion
-    fizzBuzz(fizzValue, buzzValue, limitValue);
-
+    fizzBuzz(fizzInt, buzzInt, limitInt);
   } else {
     alert("YOU MUST ENTER INTEGERS!");
   }
 }
 
 //build the array of results
-function fizzBuzz(fizzValue, buzzValue, limitValue) {
+function fizzBuzz(fizzInt, buzzInt, limitInt) {
   //init the returnArray
   let returnArray = [];
 
-  //loop from 1-vl
-  let lv = limitValue;
-  let fb = fizzValue * buzzValue;
+  //loop from 1-li
+  let li = limitInt;
+  let fb = fizzInt * buzzInt;
   let i = 1;
-  while (i <= lv) {
+  while (i <= li) {
     if (i % fb == 0) {
-      returnArray.push(`FIZZ <i class="fa-solid fa-bolt"></i> BUZZ`);
-    } else if (i % buzzValue == 0) {
-      returnArray.push(`<span">BUZZ</span>`);
-    } else if (i % fizzValue == 0) {
-      returnArray.push(`<span>FIZZ</span>`);
+      returnArray.push(`FIZZ <i  style="color: #f7e900" class="fa-solid fa-bolt"></i> BUZZ`);
+    } else if (i % buzzInt == 0) {
+      returnArray.push(`<span  style="color: red">BUZZ</span>`);
+    } else if (i % fizzInt == 0) {
+      returnArray.push(`<span  style="color: royalblue">FIZZ</span>`);
     } else {
       returnArray.push(i);
     }
@@ -48,16 +49,23 @@ function fizzBuzz(fizzValue, buzzValue, limitValue) {
   displayData(returnArray);
 }
 
-//define displayData
+//dynamically display table, width based on buzzInt
 function displayData(fbArray) {
   //loop over the array create a tablerow for each item.
   let templateRows = "";
   for (let i = 0; i < fbArray.length; i++) {
-    templateRows += `<tr><td>${fbArray[i]}</td></tr>`;
+    var z = "";
+    //loop over the row defined by buzzInt
+    for (let x = 0; x < buzzInt; x++) {
+      z += `<td>${fbArray[i+x]}</td>`;
+    }
+    templateRows += `<tr>${z}</tr>`;
+    i += buzzInt-1;
   }
   document.getElementById("results").innerHTML = templateRows;
 }
 
+//resets the table
 function resetTable() {
   let templateRows = "";
   document.getElementById("results").innerHTML = templateRows;
