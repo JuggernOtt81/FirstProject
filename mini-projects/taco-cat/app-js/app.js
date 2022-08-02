aa4e455149d10f5036741364c93fb4c55056fc2d
Lawson Ott <34386218+JuggernOtt81@users.noreply.github.com>
2022-08-02 17:46:30 -0400

    code for tacocat done, just need the wording and display page

--- mini-projects/taco-cat/app-js/app.js
+++ mini-projects/taco-cat/app-js/app.js
@@ -1,28 +1,42 @@
-//start or controller function(get the values from the page)
-var myTacoCatForm = document.getElementById('myTacoCatForm');
-var wordInput = document.getElementById('wordInput');
-var answer = document.getElementById('answer');
-
-myTacoCatForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    var userInput = wordInput.value.toLowerCase().replace(/\s|\'|\"/g, '');
-    answer.innerHTML = isPalindrome(userInput);
-
-
-    if (isPalindrome(userInput) == false) {
-        $("#answerAppend").html(userInput + " is NOT the reverse of " + wordInput.value + ".");
+//start/main/controller function
+function palindrome() {
+    let palindromeString = `not set`;
+    let uString = getValues();
+    let rString = reverseString();
+    if (rString == uString) {
+        palindromeString = `<i class="fas fa-check" style="color: green; font-size="larger;"></i>   ${reverseString()} IS a palindrome of ${getValues()}   <i class="fas fa-check" style="color: green; font-size="larger;"></i>`;
     }
     else {
-        $("#answerAppend").html(userInput + " IS the reverse of " + wordInput.value + ".");
-    }
-});
+        palindromeString = `<i class="fa-solid fa-x" style="color: red; font-size="larger;></i>   ${reverseString()} is NOT a palindrome of ${getValues()}   <i class="fa-solid fa-x" style="color: red; font-size="larger;></i>`;
+    };
+    displayString(palindromeString);
+}
 
-function isPalindrome(userInput) {
-    for (i = 0; i <= (userInput.length / 2); i++) {
-        if (userInput[i] != userInput[(userInput.length - 1 - i)]) {
-            return false;
-        }
+//get the value from the page
+function getValues() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value.toLowerCase();
+    let regex = /[^a-z0-9/]/gi;
+    userString = userString.replace(regex, ``);
+    return userString;
+}
+
+//logic function
+function reverseString() {
+    //reverse the string    
+    let uString = getValues();
+    
+    let revString = [];
+    //reverse the string using a for loop  
+    for (let index = uString.length - 1; index >= 0; index--) {
+        revString += uString[index];
     }
-    return true;
+    return revString;
+}
+
+//view function
+function displayString(palindromeString) {
+    //display the reversed string   
+    document.getElementById("msg").innerHTML = `${palindromeString}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
