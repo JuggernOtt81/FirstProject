5f8951eff4928b92d1d40be1b7cc953c95b6400e
Lawson Ott <34386218+JuggernOtt81@users.noreply.github.com>
2022-08-18 04:01:45 -0400

    added objective taco cat, but need to finish the object based code

---
+++ mini-projects/objective-taco-cat/app-js/app.js
@@ -0,0 +1,53 @@
+//start/main/controller function
+function palindrome() {
+    let palindromeString = `not set`;
+    let uString = getValues();
+    let rString = reverseString();
+    if (rString == uString) {
+        palindromeString = `<i class="fas fa-check" style="color: green; font-size="larger;"></i>   ${reverseString()} IS a palindrome of ${getValues()}   <i class="fas fa-check" style="color: green; font-size="larger;"></i>`;
+    }
+    else {
+        palindromeString = `<i class="fa-solid fa-x" style="color: red; font-size="larger;></i>   ${
+            reverseString()
+        } is NOT a palindrome of ${
+                getValues()
+            }   <i class="fa-solid fa-x" style="color: red; font-size="larger;></i>`;
+    };
+    displayString(palindromeString);
+}
+
+//get the value from the page
+function getValues() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value.toLowerCase();
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex, ``);
+    return userString;
+}
+
+//logic function
+function reverseString() {
+    //reverse the string    
+    let uString = getValues();
+    
+    let revString = [];
+    //reverse the string using a for loop  
+    for (let index = uString.length - 1; index >= 0; index--) {
+        revString += uString[index];
+    }
+    return revString;
+}
+
+/* //view function
+function displayString(palindromeString) {
+    //display the reversed string   
+    document.getElementById("msg").innerHTML = `${palindromeString}`;
+    document.getElementById("alert").classList.remove("invisible");
+} */
+
+//temporary view function
+function displayString(palindromeString) {
+    //display the reversed string   
+    document.getElementById("msg").innerHTML = `oh no! the object based version isn't done yet. please check back later. - in the meantime, have fun with the ORIGINAL Taco Cat, or other items by clicking "HOME".`;
+    document.getElementById("alert").classList.remove("invisible");
+}
