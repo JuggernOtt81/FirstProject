e6dd1dcaf5567700352bf0d6cb12cb883a58d5dc
Lawson Ott <34386218+JuggernOtt81@users.noreply.github.com>
2022-08-19 02:37:56 -0400

    TacoCat using Objects and Properties

--- mini-projects/objective-taco-cat/app-js/app.js
+++ mini-projects/objective-taco-cat/app-js/app.js
@@ -1,4 +1,83 @@
-//start/main/controller function
+function getValues(){
+    let userString = document.getElementById("userString").value;
+
+    //check palindrome
+    let returnObject = checkForPalindrome(userString);
+
+    //display message
+    displayMessage(returnObject);
+}
+
+function checkForPalindrome(userString){
+
+    userString = userString.toLowerCase();
+
+    let regex = /[^a-z0-9]/gi;
+
+    userString = userString.replace(regex,"");
+
+    let revString = [];
+    let returnObject = {};
+
+    for (let i = userString.length -1; i >= 0; i--){
+        revString += userString[i];
+    }
+
+    if (revString == userString){
+        let check = `<i class="fas fa-check" style="color: green; font-size="larger;"></i>`;
+        returnObject.header = `${check}     PALINDROME DETECTED!     ${check}`;
+        returnObject.msg = ` IS a palindrome of `
+
+    }
+    else{
+        let xMark = `<i class="fa-solid fa-x" style="color: red; font-size="larger;></i>`;
+        returnObject.header = `${xMark}     no palindrome found     ${xMark}`;
+        returnObject.msg = ` is NOT palindrome of `
+    }
+
+    returnObject.original = userString;
+    returnObject.reversed = revString;
+
+    return returnObject;
+}
+
+//display
+function displayMessage(returnObject){
+    document.getElementById("alertHeader").innerHTML = returnObject.header;
+    document.getElementById("msg").innerHTML = `<strong>${returnObject.reversed}</strong>
+    ${returnObject.msg} 
+    <strong>${returnObject.original}</strong>`;
+    document.getElementById("alert").classList.remove("invisible");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* //start/main/controller function
 function palindrome() {
     let palindromeString = `not set`;
     let uString = getValues();
@@ -38,16 +117,16 @@ function reverseString() {
     return revString;
 }
 
-/* //view function
+//view function
 function displayString(palindromeString) {
     //display the reversed string   
     document.getElementById("msg").innerHTML = `${palindromeString}`;
     document.getElementById("alert").classList.remove("invisible");
-} */
+}
 
 //temporary view function
 function displayString(palindromeString) {
     //display the reversed string   
     document.getElementById("msg").innerHTML = `oh no! the object based version isn't done yet. please check back later. - in the meantime, have fun with the ORIGINAL Taco Cat, or other items by clicking "HOME".`;
     document.getElementById("alert").classList.remove("invisible");
-}
+} */
